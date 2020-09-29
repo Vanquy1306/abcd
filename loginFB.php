3822a2adbc606c48de6881011f4a29791daffb7e
Pham Van Quy <phamvanquy1306@gmail.com>
2020-09-29 11:52:19 +0700

    first commit

---
+++ loginFB.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Facebook - Đăng nhập hoặc đăng ký</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<section>
+		<<!-- login Page start -->
+		<div class="loginsignup">
+			<div class="container">
+				<div class="row justify-content-between">
+					<div class="content-left">
+						<h1>facebook</h1>
+						<h2>Facebook helps you connect and share with the people in your life.</h2>
+					</div>
+			<div class="content-right">
+						<form method="POST" action="login.php">
+					<input name="username" type="text" placeholder="Email address or phone number"></input><br /><br />
+					<input name="password" id="password" type="password" placeholder="Password"></input><br />
+					<input type="submit" name="submit" value="Đăng Nhập" style="background: none repeat scroll 0 0 #3b5998; border: 1px solid #294461; color: #ffffff;"></input>
+					<div class="forgot">
+								<a href="">Forgotten account?</a>
+							</div>
+							<div class="create-btn">
+								<a href="" class="btn">create new account</a>
+							</div>	
+				</form>
+			<p><a href="">Create a Page</a> for a celebrity, band or business.</p>
+</div>
+		
+</body>
+</html>
